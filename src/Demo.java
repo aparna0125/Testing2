259ad62af75420fe350e5813227568a15cccf659
aparna0125 <92046186+aparna0125@users.noreply.github.com>
2021-10-10 23:39:50 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,14 +1,13 @@
 
 public class Demo {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello Git_Project2");
-		
-		public void m1()
-		{
-			System.out.println("m1 method");
-		}
-		
+	public static void main(String[] args) 
+	{	
+		System.out.println("Hello Git_Project2");		
+	}
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
 	}
 }
