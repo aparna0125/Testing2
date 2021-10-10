45fd1a4c061c866aebf62c9ddb1349e41a3b2918
ASUS <ASUS@AsusLaptop0125>
2021-10-10 23:42:24 +0530

    By A

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,10 @@ public class Demo {
 
 	public static void main(String[] args) 
 	{	
-		System.out.println("Hello Git_Project2");		
+		System.out.println("Hello Git_Project2");	
+		 
+		Demo d=new Demo();
+		d.m1();	
 	}
 	
 	public void m1()
